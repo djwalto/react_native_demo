860fefc83ec4abb10888e1f3a9612a2ed8e2929c
David Walton <davidwalton6772@gmail.com>
2020-09-28 22:45:10 -0500

    init

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { AppNavigator } from './app/app.navigator';
+
+export default class App extends Component {
+  render() {
+    return <AppNavigator />;
+  }
+}
